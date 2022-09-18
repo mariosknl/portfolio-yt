449a08e4d0f952205985688027792d9808343e01
mariosknl <mariosknl@outlook.com>
2022-09-18 09:37:16 +0300

    created: finished witht he workExperience component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import About from "../components/About";
 import { Header } from "../components/Header";
 import { Hero } from "../components/Hero";
+import WorkExperience from "../components/WorkExperience";
 
 const Home: NextPage = () => {
 	return (
@@ -15,17 +16,17 @@ const Home: NextPage = () => {
 
 			<Header />
 
-			{/* Hero */}
 			<section id="hero" className="snap-start">
 				<Hero />
 			</section>
 
-			{/* About */}
 			<section id="about" className="snap-center">
 				<About />
 			</section>
 
-			{/* Experience */}
+			<section id="experience" className="snap-center">
+				<WorkExperience />
+			</section>
 
 			{/* Skills */}
 
